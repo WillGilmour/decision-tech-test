68da9408cd452f45389cdeb90ac64cb0390bed1b
Will Gilmour <will.gilmour@drawgroup.com>
2017-05-26 17:41:40 +0100

    Adding a deals HTTP service

---
+++ src/app/services/deals.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+
+export class DealsService {
+	constructor (private http: Http) {}
+
+	getDeals(): Observable <any> {
+		debugger;
+	   return this.http.get('assets/deals.json')
+      	.map(res => res.json())
+	}
+}
