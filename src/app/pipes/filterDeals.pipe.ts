a1eeb4764187716e0c83bbd91b25e6aadda33569
Will Gilmour <will.gilmour@drawgroup.com>
2017-05-29 11:10:51 +0100

    Deal filter pipe - product type filtering functionality

--- src/app/pipes/filterDeals.pipe.ts
+++ src/app/pipes/filterDeals.pipe.ts
@@ -14,7 +14,44 @@ export class FilterDealsPipe implements PipeTransform {
 	}
 	
 	transform(allDeals) {
-		return allDeals
+		return this.filter(allDeals)
+	}
+
+	private filter(array) {
+		let testProductTypes = this.filtersService.filters;
+
+		if(this.checkFilters(testProductTypes)){
+			return array;
+		}
+
+		return array.filter((item) => {
+			return this.testMultipleProductTypes(item.productTypes, testProductTypes)
+		});
+	}
+
+	private checkFilters(productTypeStrings){
+		
+		let filtersAllFalse = <boolean> true;
+
+		for (var id in productTypeStrings){
+			filtersAllFalse = filtersAllFalse && !productTypeStrings[id];
+		}
+
+		return filtersAllFalse;
+	}
+
+	private testProductType(productTypes, productTypeString) {
+		return productTypes.indexOf(productTypeString) > -1;
+	}
+
+	private testMultipleProductTypes(productTypes, productTypeStrings){
+		let passes = <boolean> true;
+
+		for (var id in productTypeStrings){
+			passes = passes && (this.testProductType(productTypes, id) === productTypeStrings[id]);
+		}
+
+		return passes;		
 	}
 
 }
